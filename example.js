82d3a5f5260932a0e7e33e1229dd798cfd09e3f8
Tofandel <adrien.foulon@tukan.hu>
2024-07-04 16:44:42 +0200

    Migrate to typescript and esm

--- example.js
+++ example.js
@@ -1,42 +1,45 @@
-const debounce = require('debounce-promise')
+const debounce = require("debounce-promise");
 
-function expensiveOperation (value) {
-  return Promise.resolve(value)
+function expensiveOperation(value) {
+	return Promise.resolve(value);
 }
 
 // Simple example
 {
-  const saveCycles = debounce(expensiveOperation, 100);
+	const saveCycles = debounce(expensiveOperation, 100);
 
-  [1, 2, 3, 4].forEach(num => {
-    return saveCycles('call no #' + num).then(value => {
-      console.log(value)
-    })
-  })
+	[1, 2, 3, 4].forEach((num) => {
+		return saveCycles(`call no #${num}`).then((value) => {
+			console.log(value);
+		});
+	});
 }
 
 // With leading=true & trailing=false
 {
-  const saveCycles = debounce(expensiveOperation, 100, { leading: true, trailing: false });
+	const saveCycles = debounce(expensiveOperation, 100, {
+		leading: true,
+		trailing: false,
+	});
 
-  [1, 2, 3, 4].forEach(num => {
-    return saveCycles('call no #' + num).then(value => {
-      console.log(value)
-    })
-  })
+	[1, 2, 3, 4].forEach((num) => {
+		return saveCycles(`call no #${num}`).then((value) => {
+			console.log(value);
+		});
+	});
 }
 
 // With accumulate=true
 {
-  function squareValues (values) {
-    return Promise.all(values.map(val => val * val))
-  }
+	function squareValues(values) {
+		return Promise.all(values.map((val) => val * val));
+	}
 
-  const square = debounce(squareValues, 100, { accumulate: true });
+	const square = debounce(squareValues, 100, { accumulate: true });
 
-  [1, 2, 3, 4].forEach(num => {
-    return square(num).then(value => {
-      console.log(value)
-    })
-  })
+	[1, 2, 3, 4].forEach((num) => {
+		return square(num).then((value) => {
+			console.log(value);
+		});
+	});
 }
---
+++ index.ts
@@ -0,0 +1,150 @@
+export interface Options {
+	leading?: boolean;
+	trailing?: boolean;
+	accumulate?: boolean;
+}
+
+interface Deferred<PromiseReturn> {
+	promise: Promise<Awaited<PromiseReturn>>;
+	resolve: (value: unknown) => void;
+	reject: (reason: unknown) => void;
+}
+
+type Wait = number | (() => number);
+
+function debounce<
+	FunctionType extends (args: unknown[]) => Array<unknown> | Promise<Array<unknown>>,
+>(
+	fn: FunctionType,
+	wait?: Wait,
+	opts?: Options & { accumulate: true },
+):
+	{ flush: () => void; clear: () => void }
+	& ((
+	...args: Parameters<FunctionType>[0]
+) => Promise<Awaited<ReturnType<FunctionType>>[0]>)
+
+function debounce<
+	FunctionType extends (...args: unknown[]) => unknown | Promise<unknown>,
+>(
+	fn: FunctionType,
+	wait?: Wait,
+	opts?: Options & { accumulate?: false },
+):
+	{ flush: () => void; clear: () => void }
+	& ((
+	...args: Parameters<FunctionType>
+) => Promise<Awaited<ReturnType<FunctionType>>>)
+
+function debounce<
+	FunctionType extends (...args: unknown[]) => unknown | Promise<unknown>,
+>(
+	fn: FunctionType,
+	wait: Wait = 0,
+	opts: Options = {},
+):
+	{ flush: () => void; clear: () => void }
+	& ((
+			...args: Parameters<FunctionType>
+	  ) => Promise<Awaited<ReturnType<FunctionType>>>) {
+	let lastCallAt: number;
+	let deferred: Deferred<
+		ReturnType<FunctionType> | Array<ReturnType<FunctionType>>
+	> | null;
+	let firstPromise: Promise<Awaited<ReturnType<FunctionType>>>;
+	let timer: NodeJS.Timeout;
+	let pendingArgs: unknown[] = [];
+	const options: Required<Options> = {
+		leading: false,
+		trailing: true,
+		accumulate: false,
+		...opts,
+	};
+
+	const ret = function debounced(
+		...args: unknown[]
+	): Promise<Awaited<ReturnType<FunctionType>>> {
+		const currentWait = getWait(wait);
+		const currentTime = new Date().getTime();
+
+		const isCold =
+			lastCallAt === undefined || currentTime - lastCallAt > currentWait;
+
+		lastCallAt = currentTime;
+
+		if (isCold && options.leading) {
+			firstPromise = options.accumulate
+				? Promise.resolve(fn.call(this, [args])).then((result) => result[0])
+				: Promise.resolve(fn.call(this, ...args));
+			return firstPromise;
+		}
+
+		if (deferred != null) {
+			clearTimeout(timer);
+		} else {
+			deferred = defer();
+		}
+
+		if (options.trailing) {
+			pendingArgs.push(args);
+			timer = setTimeout(flush.bind(this), currentWait);
+
+			if (options.accumulate) {
+				const argsIndex = pendingArgs.length - 1;
+				return deferred.promise.then(
+					(results: Array<Awaited<ReturnType<FunctionType>>>) =>
+						results[argsIndex],
+				);
+			}
+		} else {
+			return firstPromise;
+		}
+
+		return deferred.promise as Promise<Awaited<ReturnType<FunctionType>>>;
+	};
+
+	function clear(): void {
+		if (timer !== undefined) {
+			clearTimeout(timer);
+			pendingArgs = [];
+		}
+		deferred = null;
+	}
+
+	function flush(): void {
+		clearTimeout(timer);
+
+		const p = Promise.resolve(
+			options.accumulate
+				? fn.call(this, pendingArgs)
+				: fn.apply(this, pendingArgs[pendingArgs.length - 1]),
+		);
+		console.log(deferred)
+		if (deferred != null) {
+			p.then(deferred.resolve, deferred.reject);
+		}
+
+		pendingArgs = [];
+		deferred = null;
+	}
+
+	ret.clear = clear;
+	ret.flush = flush;
+
+	return ret;
+}
+
+function getWait(wait: Wait): number {
+	return typeof wait === "function" ? wait() : wait;
+}
+
+function defer<ReturnType>(): Deferred<ReturnType> {
+	const deferred: Partial<Deferred<ReturnType>> = {};
+	deferred.promise = new Promise((resolve, reject) => {
+		deferred.resolve = resolve;
+		deferred.reject = reject;
+	});
+	return deferred as Deferred<ReturnType>;
+}
+
+export default debounce;
